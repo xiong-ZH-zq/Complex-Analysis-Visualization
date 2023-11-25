ae54a587a6572c30dbbb4bdcb6ca460caaca3ac3
xzqbear <68729283+xiong-ZH-zq@users.noreply.github.com>
2023-11-25 19:43:15 +0800

    Update Julia Set.py

--- Complex Fractals/Julia Set.py
+++ Complex Fractals/Julia Set.py
@@ -17,25 +17,25 @@ def julia_set(Z, c, max_iter):
         out[absZ>2] = n
         Z = Z*Z + c
     return out
-
-xmin, xmax, ymin, ymax = -2, 2, -2, 2
-npoints = 800
-max_iter = 100
-c = complex(0.285, 0.01)
-
-x = torch.linspace(xmin, xmax, npoints)
-y = torch.linspace(ymin, ymax, npoints)
-X, Y = torch.meshgrid(x, y)
-Z = X + 1j*Y
-
-# 生成 Julia 集
-julia = julia_set(Z, c, max_iter)
-
-# 显示 Julia 集的图像
-fig = plt.figure(dpi = 480)
-fig.subplots_adjust(top=1, bottom=0, left=0, right=1)
-ax = plt.subplot()
-plt.imshow(julia, cmap="hot")
-plt.axis("off")
-# plt.savefig(fname = "./Julia Set.png",dpi = 960)
-plt.show()
+if __name__ == '__main__':
+    xmin, xmax, ymin, ymax = -2, 2, -2, 2
+    npoints = 800
+    max_iter = 100
+    c = complex(0.285, 0.01)
+    
+    x = torch.linspace(xmin, xmax, npoints)
+    y = torch.linspace(ymin, ymax, npoints)
+    X, Y = torch.meshgrid(x, y)
+    Z = X + 1j*Y
+    
+    # 生成 Julia 集
+    julia = julia_set(Z, c, max_iter)
+    
+    # 显示 Julia 集的图像
+    fig = plt.figure(dpi = 480)
+    fig.subplots_adjust(top=1, bottom=0, left=0, right=1)
+    ax = plt.subplot()
+    plt.imshow(julia, cmap="hot")
+    plt.axis("off")
+    # plt.savefig(fname = "./Julia Set.png",dpi = 960)
+    plt.show()
